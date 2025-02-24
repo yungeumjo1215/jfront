c78317b8c3060f261e35990d64800ff3960cb6aa
yungeumjo1215 <elec0807@gmail.com>
2025-02-25 01:21:18 +0900

    dd

--- backend/models/Company.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const companySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  logo: { type: String },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Company', companySchema); 
